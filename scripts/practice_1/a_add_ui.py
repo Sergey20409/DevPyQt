d81f184e64514342b8959a2af8959b124b3b16ca
trakt <traktorsergey@gmail.com>
2025-05-07 00:11:01 +0300

    06.05.2025

--- scripts/practice_1/a_add_ui.py
+++ scripts/practice_1/a_add_ui.py
@@ -7,16 +7,19 @@ PySide6-uic path_to_form.ui -o path_to_form.py
 
 from PySide6 import QtWidgets
 
-from ui.a_add_ui_form import Ui_MainWindow  # Импортируем класс формы
+from ui.a_add_ui_form import Ui_Dialog  # Импортируем класс формы
 
 
-class Window(QtWidgets.QMainWindow):  # наследуемся от того же класса, что и форма в QtDesigner
+class Window(QtWidgets.QWidget):  # наследуемся от того же класса, что и форма в QtDesigner
     def __init__(self, parent=None):
         super().__init__(parent)
 
         # Создание "прокси" переменной для работы с формой
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
+        self.setWindowTitle("Вход")
+        self.ui.pushButtonForget.setText("Забыли пароль ?!!?")
 
 
 if __name__ == "__main__":

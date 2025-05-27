acf8034dc68c47d9ccba9d91051e770608bed11e
trakt <traktorsergey@gmail.com>
2025-05-27 22:37:56 +0300

    06.05.2025

--- scripts/practice_2/a_repeat/b_events.py
+++ scripts/practice_2/a_repeat/b_events.py
@@ -1,14 +1,14 @@
 """
-Файл для повторения темы событий
+Файл для повторения темы сигналов
 
-Напомнить про работу с событиями.
+Напомнить про работу с сигналами и изменением Ui.
 
-Предлагается создать приложение, которое будет показывать все события происходящие в приложении,
-(переопределить метод event), вывод событий производить в консоль.
-При выводе события указывать время, когда произошло событие.
+Предлагается создать приложение, которое принимает в lineEditInput строку от пользователя,
+и при нажатии на pushButtonMirror отображает в lineEditMirror введённую строку в обратном
+порядке (задом наперед).
 """
 
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets
 
 
 class Window(QtWidgets.QWidget):
@@ -16,8 +16,46 @@ class Window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-    def event(self, event: QtCore.QEvent):
-        pass
+        self.__initUi()
+        self.__initSignals()
+
+    def __initUi(self):
+        self.lineEditInput = QtWidgets.QLineEdit()
+        self.lineEditInput.setPlaceholderText("Введите фразу")
+
+        self.lineEditMirror = QtWidgets.QLineEdit()
+
+        self.pushButtonMirror = QtWidgets.QPushButton('Mirror')
+        self.pushButtonClear = QtWidgets.QPushButton('Clear')
+
+        l_lineEdit = QtWidgets.QHBoxLayout()
+        l_lineEdit.addWidget(self.lineEditInput)
+        l_lineEdit.addWidget(self.lineEditMirror)
+
+        l_pushButton = QtWidgets.QHBoxLayout()
+        l_pushButton.addWidget(self.pushButtonMirror)
+        l_pushButton.addWidget(self.pushButtonClear)
+
+        l_main = QtWidgets.QVBoxLayout()
+        l_main.addLayout(l_lineEdit)
+        l_main.addLayout(l_pushButton)
+
+        self.setLayout(l_main)
+
+    def __initSignals(self):
+        self.pushButtonMirror.clicked.connect(self.__onPushButtonMirrorClicked)
+        self.pushButtonClear.clicked.connect(self.__onPushButtonClearClicked)
+        self.lineEditInput.textChanged.connect(self.__onLineEditMirrorTextChanged)
+
+    def __onPushButtonMirrorClicked(self):
+        self.lineEditMirror.setText(self.lineEditInput.text()[::-1])
+
+    def __onPushButtonClearClicked(self):
+        self.lineEditMirror.clear()
+        self.lineEditInput.clear()
+
+    def __onLineEditMirrorTextChanged(self, text):
+        self.lineEditMirror.setText(text[::-1])
 
 
 if __name__ == "__main__":
